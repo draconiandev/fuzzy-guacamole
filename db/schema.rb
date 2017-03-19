8bdd50867c6ea5d331b55e6d2a3fd1b2980c9194
p150 <pavanprakash21@gmail.com>
2017-03-19 14:29:30 +0530

    add tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319083411) do
+ActiveRecord::Schema.define(version: 20170319085850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20170319083411) do
     t.index ["status"], name: "index_posts_on_status", using: :gin
     t.index ["title"], name: "index_posts_on_title", using: :gin
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
